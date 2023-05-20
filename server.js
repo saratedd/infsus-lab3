48fb6c97f93fb6bba40983e093af2d3c073ece62
saratedd <sara.tedesko91@gmail.com>
2023-05-20 12:04:59 +0200

    start

--- server.js
+++ server.js
@@ -2,15 +2,31 @@
 const express = require('express');
 const app = express();
 
-// const api = require('./routes/api.route');
 var format = require('pg-format');
 const pool = require('./db/pgadmin');
-// const { auth } = require('express-openid-connect');
-// const Zip = require("adm-zip");
-// require('dotenv').config();
+
 
 app.set('view engine', 'ejs');
 // to serve static files
 app.use(express.static(__dirname));
 // da mozemo accessat stvari iz bodya
 app.use(express.urlencoded({extended: true}));
+
+
+var attributes_korisnik = ['oib', 'isadmin', 'ime', 'prezime', 'email', 'lozinka', 'idzaduzenja']
+var attributes_narudzba = ['idnarudzbe', 'idstola', 'idracuna', 'idstatusa']
+
+
+app.get('/', async (req, res) => {
+    let search = `select * from narudzba`
+    var result = await pool.query(search)
+
+    res.render('index', {
+        text: 'test',
+        rows: result.rows,
+        attributes: attributes_narudzba,
+    });
+});
+
+
+app.listen(3000);
